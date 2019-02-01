054ac0670f18c38b7d09ac4316e2c15473958447
tone <tturner@pacificmetrics.com>
2019-02-01 13:23:49 -0700

    Returning correctly

--- config/default/saveCounter.conf.php
+++ config/default/saveCounter.conf.php
@@ -1,20 +1,3 @@
 <?php
 
-return new oat\oatbox\log\LoggerService(array(
-    'logger' => new \oat\oatbox\log\LoggerAggregator(
-        array(
-
-            new oat\oatbox\log\logger\TaoLog(array(
-                'appenders' => array(
-                    array(
-                        'class' => 'UDPAppender',
-                        'host' => '127.0.0.1',
-                        'port' => 5775,
-                        'threshold' => 0,
-                        'prefix' => 'tao'
-                    )
-                )
-            )),
-        )
-    )
-));
+return new \awesomeExtension\model\SaveCounterService();
